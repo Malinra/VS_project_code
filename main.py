f59b1bc5d599f7029493852790b2b45c3ead0090
feifan.ma <feifan.ma@nio.com>
2022-10-19 22:17:53 +0800

    更改

--- main.py
+++ main.py
@@ -12,6 +12,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print_hi('我只是一个程序')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
